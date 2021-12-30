bcfb8ed2ab778e4e8fb17e22d00065b280472806
Veremeyenko Maksym <verem@m1.tv>
2021-12-30 13:01:19 +0200

    Enable pullup resistors on STM's pins

--- STM32/STM32F411CEU6/Core/Src/main.c
+++ STM32/STM32F411CEU6/Core/Src/main.c
@@ -258,7 +258,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : PB8 PB9 */
   GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP; //GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
