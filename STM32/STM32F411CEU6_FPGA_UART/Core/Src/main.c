120bbcd382faa4cb9f4a435a8a28b5ff54b1ca0c
Veremeyenko Maksym <verem@m1.tv>
2022-01-04 23:07:44 +0200

    Implement serial receive from FPGA, rise frequencies

--- STM32/STM32F411CEU6_FPGA_UART/Core/Src/main.c
+++ STM32/STM32F411CEU6_FPGA_UART/Core/Src/main.c
@@ -62,14 +62,88 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 static struct
 {
 	uint8_t s1, s2, rot;
-	int32_t value;
+	int32_t value1, value2, value3;
 } instance;
+
+#define PACKET_STH	0x5a
+#define PACKET_LEN	11
+#define PACKET_REQ	1
+
+static int rx_length = 0;
+static uint8_t rx_queue[PACKET_LEN];
+static uint8_t rx_buffers[2][PACKET_REQ];
+static int rx_buf_idx = 0;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	int i, j;
+	uint8_t* rx_buffer = rx_buffers[rx_buf_idx++];
+
+	/* receive more */
+	HAL_UART_Receive_IT(&huart2, rx_buffers[rx_buf_idx], PACKET_REQ);
+
+	/* increment byte counter */
+	instance.s1 += PACKET_REQ;
+
+	for(i = 0; i < PACKET_REQ; i++)
+	{
+		if(rx_length < PACKET_STH)
+		{
+			rx_queue[rx_length] = rx_buffer[i];
+			rx_length++;
+		}
+		else
+		{
+			for(j = 0; j < (PACKET_LEN - 1); j++)
+				rx_queue[j] = rx_queue[j + 1];
+			rx_queue[PACKET_LEN - 1] = rx_buffer[i];
+		};
+
+		/* check packet size and STH */
+		if(rx_length == PACKET_STH && rx_queue[0] == PACKET_STH)
+		{
+			uint8_t cs;
+
+			/* calc checksum */
+			for(cs = 0, j = 0; j < (PACKET_LEN - 1); j++)
+                cs += rx_queue[j];
+            if(rx_queue[PACKET_LEN - 1] == cs)
+            {
+            	int32_t v;
+
+                v = 0;
+                v |= rx_queue[3]; v <<= 8;
+                v |= rx_queue[2]; v <<= 8;
+                v |= rx_queue[1];
+                if(v & 0x00800000) v |= 0xFF000000;
+            	instance.value1 = v;
+
+                v = 0;
+                v |= rx_queue[6]; v <<= 8;
+                v |= rx_queue[5]; v <<= 8;
+                v |= rx_queue[4];
+                if(v & 0x00800000) v |= 0xFF000000;
+            	instance.value2 = v;
+
+                v = 0;
+                v |= rx_queue[9]; v <<= 8;
+                v |= rx_queue[8]; v <<= 8;
+                v |= rx_queue[7];
+                if(v & 0x00800000) v |= 0xFF000000;
+            	instance.value1 = v;
+
+            	/* increment packets counter */
+            	instance.s2++;
+            }
+		}
+	};
+}
 
 int8_t CUSTOM_HID_OutEvent_FS_main(uint8_t* buf)
 {
 	instance.s1 = buf[0];
 	instance.s2 = buf[1];
-
+/*
 	instance.value = buf[2];
 	instance.value <<= 8;
 	instance.value |= buf[3];
@@ -77,60 +151,8 @@ int8_t CUSTOM_HID_OutEvent_FS_main(uint8_t* buf)
 	instance.value |= buf[4];
 	instance.value <<= 8;
 	instance.value |= buf[5];
-
+*/
 	return (USBD_OK);
-};
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	if(GPIO_Pin != GPIO_PIN_9 && GPIO_Pin != GPIO_PIN_8)
-		return;
-
-/*
- * CW
- *
- * 00	10
- * 01	00
- * 10	11
- * 11	01
- *
- * CCW
- *
- * 00	01
- * 01	11
- * 10	00
- * 11	10
- *
- * MAP
- *
- * 00	00	 0
- * 00	01	-1
- * 00	10	+1
- * 00	11	 0
- * 01	00	+1
- * 01	01	 0
- * 01	10	 0
- * 01	11	-1
- * 10	00	-1
- * 10	01	 0
- * 10	10	 0
- * 10	11	+1
- * 11	00	 0
- * 11	01	+1
- * 11	10	-1
- * 11	11	 0
- *
- */
-
-	static const int8_t r[16] = { 0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0 };
-
-
-	instance.rot <<= 1;
-	instance.rot |= HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_8);
-	instance.rot <<= 1;
-	instance.rot |= HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_9);
-
-	instance.value += r[ instance.rot & 0x0F ];
 };
 
 /* USER CODE END 0 */
@@ -166,6 +188,7 @@ int main(void)
   MX_USB_DEVICE_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_IT(&huart2, rx_buffers[rx_buf_idx], PACKET_REQ);	// start UART receiving
 
   /* USER CODE END 2 */
 
@@ -173,18 +196,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  uint8_t buf[8];
+	  uint8_t buf[11];
+	  int32_t values[3] = {instance.value1, instance.value2, instance.value3};
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
 	  buf[0] = instance.s1;
 	  buf[1] = instance.s2;
-	  buf[2] = instance.value >> 24;
-	  buf[3] = instance.value >> 16;
-	  buf[4] = instance.value >>  8;
-	  buf[5] = instance.value >>  0;
 
-	  USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, buf, 6);
+	  buf[2] = values[0] >> 16;
+	  buf[3] = values[0] >>  8;
+	  buf[4] = values[0] >>  0;
+
+	  buf[5] = values[1] >> 16;
+	  buf[6] = values[1] >>  8;
+	  buf[7] = values[1] >>  0;
+
+	  buf[8] = values[2] >> 16;
+	  buf[9] = values[2] >>  8;
+	  buf[10] = values[2] >>  0;
+
+	  USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, buf, 11);
 	  HAL_Delay(10);
 
 	  if(!(led_cnt % 10))
@@ -210,16 +242,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 15;
-  RCC_OscInitStruct.PLL.PLLN = 144;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 5;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -228,12 +258,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
