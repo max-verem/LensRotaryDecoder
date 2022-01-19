c66e485218d99cdd52c534b7db6a8e64d00a2aa1
Veremeyenko Maksym <verem@m1.tv>
2022-01-19 18:13:57 +0200

    Enable IRQ for UART, setup 230400 baudrate

--- STM32/STM32F411CEU6_RotaryDecoder_3X/Core/Inc/stm32f4xx_it.h
+++ STM32/STM32F411CEU6_RotaryDecoder_3X/Core/Inc/stm32f4xx_it.h
@@ -57,6 +57,7 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void RCC_IRQHandler(void);
+void USART1_IRQHandler(void);
 void OTG_FS_IRQHandler(void);
 void DMA2_Stream5_IRQHandler(void);
 void DMA2_Stream7_IRQHandler(void);
--- STM32/STM32F411CEU6_RotaryDecoder_3X/Core/Src/stm32f4xx_it.c
+++ STM32/STM32F411CEU6_RotaryDecoder_3X/Core/Src/stm32f4xx_it.c
@@ -59,6 +59,7 @@
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 extern DMA_HandleTypeDef hdma_tim1_up;
 extern DMA_HandleTypeDef hdma_usart1_tx;
+extern UART_HandleTypeDef huart1;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -212,6 +213,20 @@ void RCC_IRQHandler(void)
   /* USER CODE BEGIN RCC_IRQn 1 */
 
   /* USER CODE END RCC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
 }
 
 /**
