91d42db00c116e48b691637810f2778050965b85
Maksym Veremeyenko <verem@internal.m1stereo.tv>
2025-04-06 20:59:22 +0300

    Added updated STM32F411CEU6_RotaryDecoder_3X

---
+++ STM32/STM32F411CEU6_RotaryDecoder_3X/Core/Src/app.h
@@ -0,0 +1,26 @@
+#ifndef SRC_APP_H_
+#define SRC_APP_H_
+
+#define ROTARY_TABLE
+
+int app_init();
+int app_begin();
+int app_loop();
+
+#define DMA_BUF_SIZE        400	// 4MHz / 400 = 10KHz
+#define TIMER_PRESCALER     8 	// 96MHz / 8 = 12Mhz
+#define TIMER_PERIOD        2 	// 12Mhz / 2 = 6MHz
+#define SERIAL_OUTPUT_TEXT	// This defines if UART1 sent with human readable text or binary formatted packet
+
+/*
+NB:
+
+[...]
+  htim1.Init.Prescaler = TIMER_PRESCALER - 1;
+[...]
+  htim1.Init.Period = TIMER_PERIOD - 1;
+[...]
+
+ */
+
+#endif /* SRC_APP_H_ */
